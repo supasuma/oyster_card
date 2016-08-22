ac7f739885c9c7f73b9a482bf7ebc1d301a0cd15
naomi <naomi.emma.spencer@gmail.com>
2016-08-22 17:50:45 +0100

    Challange #06: implimented maximum limit

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,14 +1,15 @@
 class Oystercard
-  DEFAULT_BALANCE = 0
 
-  def initialize(balance = DEFAULT_BALANCE)
-    @balance = balance
+MAXIMUM_LIMIT = 90
+
+attr_reader :balance
+
+  def initialize
+    @balance = 0
   end
 
   def top_up(amount)
-    @balance += amount
+    fail 'Top up limited exceeded' if amount + balance > MAXIMUM_LIMIT
+    @balance = amount
   end
-
-  attr_reader :balance
-
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -9,5 +9,11 @@ describe Oystercard do
     it 'tops up Oystercard by amount 20' do
       expect { subject.top_up(20) }.to change{subject.balance}.by(20)
     end
+
+    it 'raises an error when top up limit is exceeded' do
+      maximum_limit = Oystercard::MAXIMUM_LIMIT
+      subject.top_up(maximum_limit)
+      expect{ subject.top_up 1 }.to raise_error 'Top up limited exceeded'
+    end
   end
 end
