da530a0974a299e93384e9830150cefb1c754de8
naomi <naomi.emma.spencer@gmail.com>
2016-08-22 16:32:12 +0100

    Completed challege #04: balance method added to Oystercard class

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,3 +1,10 @@
 class Oystercard
+  DEFAULT_BALANCE = 0
+
+  def initialize(balance = DEFAULT_BALANCE)
+    @balance = balance
+  end
+
+  attr_reader :balance
 
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,5 +1,7 @@
 require 'oystercard'
 
 describe Oystercard do
-
+  it 'Describes an account when it is first opened' do
+    expect(subject.balance).to eq (0)
+  end
 end
