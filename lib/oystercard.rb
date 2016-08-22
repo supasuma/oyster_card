c6cebe85a571968606039bd5726c57bc51f3945b
supasuma <supasuma@gmail.com>
2016-08-22 15:57:46 +0100

    #6 adding limit

---
+++ lib/oystercard.rb
@@ -0,0 +1,19 @@
+class Oystercard
+
+  attr_reader :balance
+  BALANCE_LIMIT = 90
+
+  def initialize
+    @balance = 0
+  end
+
+  def top_up(value)
+    fail "Your top up will exceed balance limit of #{BALANCE_LIMIT}!" if value + balance > BALANCE_LIMIT
+    @balance =+ value
+
+  end
+
+
+
+#"Your balance is #{balance}"
+end
