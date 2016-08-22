3d6f69c8107baf3d90f11625c58208ad3af7b1f8
BenRoss92 <benjaminjackross@gmail.com>
2016-08-22 16:07:01 +0100

    touching in changes in_journey status to true

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -14,4 +14,16 @@ class Oystercard
     @balance += amount
   end
 
+  def deduct(amount)
+    @balance -= amount
+  end
+
+  def in_journey?
+    @in_journey
+  end
+
+  def touch_in
+    @in_journey = true
+  end
+
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -23,9 +23,24 @@ describe Oystercard do
         message = "balance cannot exceed #{Oystercard::DEFAULT_MAX}"
         expect{oyster.top_up(Oystercard::DEFAULT_MAX+10)}.to raise_error message
       end
+    end
+  end
 
+  describe '#deduct' do
+
+    it 'reduces balance by amount' do
+      oyster.top_up(40)
+      oyster.deduct(10)
+      expect(oyster.balance).to eq(30)
     end
 
+  end
+
+  it { is_expected.to respond_to(:in_journey?)}
+
+  it 'when touching in, in journey equals true' do
+    oyster.touch_in
+    expect(oyster.in_journey?).to be(true)
   end
 
 end
