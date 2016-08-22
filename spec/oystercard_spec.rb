4d4364cf4a10abd9214643e360a03d9523f98e93
BenRoss92 <benjaminjackross@gmail.com>
2016-08-22 14:51:45 +0100

    oyster initializes with 0 balance

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,3 +1,11 @@
+require 'oystercard'
+
 describe Oystercard do
+
+  subject(:oyster) { described_class.new }
+
+  it 'initializes with a balance of 0' do
+    expect(oyster.balance).to eq(0)
+  end
 
 end
