1a249ba6fd6a63e1cd564a7a7f73479ad18abef8
Peter Miklos <petermiklos@Peters-MacBook-Pro.local>
2016-08-22 13:35:45 +0100

    complete step 4

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,3 +1,13 @@
 class Oystercard
 
+  #attr_reader :balance
+
+  #def initialize
+  #  @balance = 0
+  #end
+
+  def balance
+    0
+  end
+
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -2,4 +2,12 @@ require 'oystercard'
 
 describe Oystercard do
 
+  subject(:card) {described_class.new}
+
+  describe "initialize" do
+    it "creates an instance of the card and the balance should be zero" do
+      expect(card.balance).to eq 0
+    end
+  end
+
 end
