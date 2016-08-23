b52a0f505c0016ff9ec0adaf3aa8f4d0d149dbc6
Lawrence Dawson <lawrencedawson@hotmail.co.uk>
2016-08-23 16:30:53 +0100

    added test for exit station in journey class

--- lib/journey.rb
+++ lib/journey.rb
@@ -1,4 +1,11 @@
 class Journey
 
+attr_reader :entry_station
+
+
+  def initialize(entry_station)
+    @entry_station = entry_station
+
+  end
 
 end
--- spec/journey_spec.rb
+++ spec/journey_spec.rb
@@ -2,11 +2,12 @@ require 'journey'
 
 describe Journey do
 
-  subject(:journey) { described_class.new }
+let(:entry_station) { double :station }
+let(:exit_station) { double :station}
 
   describe '#given an entry station' do
 
-  let(:entry_station) { double :station }
+    subject(:journey) { described_class.new(entry_station) }
 
     it 'should have an entry station' do
       expect(journey.entry_station).to eq entry_station
@@ -14,4 +15,10 @@ describe Journey do
 
   end
 
+  describe '#given an exit station' do
+    subject(:journey) { described_class.new(entry_station) }
+    it 'should have an exit station' do
+      expect(journey.exit_station).to eq exit_station
+    end
+  end
 end
