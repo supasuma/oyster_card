0728db8e086e5054befc0bf862cbfcf11803df9a
Abigail <abigail.mcphillips@gmail.com>
2016-08-23 17:00:14 +0100

    tests that finish method returns exit station

--- lib/journey.rb
+++ lib/journey.rb
@@ -1,11 +1,11 @@
 class Journey
 
-attr_reader :entry_station
+attr_reader :entry_station, :exit_station
 
 
-  def initialize(entry_station)
+  def initialize(entry_station,exit_station)
     @entry_station = entry_station
-
+    @exit_station = exit_station
   end
 
 end
--- spec/journey_spec.rb
+++ spec/journey_spec.rb
@@ -2,12 +2,11 @@ require 'journey'
 
 describe Journey do
 
+subject(:journey) { described_class.new(entry_station, exit_station) }
 let(:entry_station) { double :station }
-let(:exit_station) { double :station}
+let(:exit_station) { double :station }
 
   describe '#given an entry station' do
-
-    subject(:journey) { described_class.new(entry_station) }
 
     it 'should have an entry station' do
       expect(journey.entry_station).to eq entry_station
@@ -16,9 +15,20 @@ let(:exit_station) { double :station}
   end
 
   describe '#given an exit station' do
-    subject(:journey) { described_class.new(entry_station) }
+
     it 'should have an exit station' do
       expect(journey.exit_station).to eq exit_station
     end
+
   end
+
+  describe '#finish' do
+
+    it 'should return the exit station' do
+      journey.finish(exit_station)
+      expect(journey.exit_station).to eq exit_station
+    end
+
+  end
+
 end
