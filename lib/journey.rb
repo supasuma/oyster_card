6685c017b989c2400a8f4102723fe262b5b1ad56
MrJons <shad_stick@hotmail.co.uk>
2016-08-24 14:57:57 +0100

    tied in fare charging.

--- lib/journey.rb
+++ lib/journey.rb
@@ -2,13 +2,30 @@ class Journey
 
   attr_reader :entry_station
 
-  def initialize(entry_station)
+  MINIMUM_FARE = 1
+  PENALTY_FARE = 6
+
+  def initialize(entry_station = nil)
     @entry_station = entry_station
+    @exit_station = nil
   end
 
-  def finish_journey(exit_station)
-    {:entry_station => @entry_station , :exit_station => exit_station}
+  def finish_journey(exit_station = @exit_station)
+    @exit_station = exit_station
+    @journey = {:entry_station => @entry_station , :exit_station => exit_station}
   end
+
+  def fare
+    #PENALTY_FARE unless @entry_station == nil
+    if @exit_station == nil || @entry_station == nil
+      PENALTY_FARE
+    else
+      MINIMUM_FARE
+    end
+  end
+
+  #How do we create an instance where there is no touch in???
+
 
   # def exit_station(exit_station)
   #   @exit_station = exit_station
--- spec/journey_spec.rb
+++ spec/journey_spec.rb
@@ -6,6 +6,7 @@ require 'journey'
 describe Journey do
 
   subject(:journey) { described_class.new ("Bank") }
+  subject(:journey_nil) {described_class.new}
   let(:card) { double :oystercard }
 
   describe '#initialize' do
@@ -17,6 +18,21 @@ describe Journey do
   describe '#finish_journey' do
     it 'records total journey' do
       expect(journey.finish_journey("Aldgate")).to eq({ :entry_station => "Bank" , :exit_station => "Aldgate" })
+    end
+  end
+
+  describe '#fare' do
+    it "charges mimumum fare on full journey" do
+      journey.finish_journey("Aldgate")
+      expect(journey.fare).to eq(Journey::MINIMUM_FARE)
+    end
+
+    it "charges penalty fare on missing exit station" do
+      expect(journey.fare).to eq(Journey::PENALTY_FARE)
+    end
+
+    it "charges penalty fare on missing entry station" do
+      expect(journey_nil.fare).to eq(Journey::PENALTY_FARE)
     end
   end
 
