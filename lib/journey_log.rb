b6102a4a126ae707baf22e277889f168b982a253
supasuma <supasuma@gmail.com>
2016-08-25 15:31:23 +0100

    added finish method

--- lib/journey_log.rb
+++ lib/journey_log.rb
@@ -3,15 +3,33 @@ require_relative 'journey'
 class JourneyLog
 
 
+
   def initialize(journey_class: Journey)
     @journey_class = journey_class
+    @journey = nil
+
   end
 
   def start(entry_station)
-    @journey_class.new(entry_station)
+    @entry_station = entry_station
+    @journey = current_journey(entry_station)
   end
 
+  def finish(exit_station)
+    @exit_station = exit_station
+    @journey = {:entry_station => @entry_station , :exit_station => exit_station}
+  end
 
+private
 
+attr_reader :current_journey
+
+ def current_journey(entry_station)
+   if @journey.nil?
+     @journey_class.new(entry_station)
+   else
+    @journey
+   end
+ end
 
 end
--- spec/journey_log_spec.rb
+++ spec/journey_log_spec.rb
@@ -3,13 +3,24 @@ require 'journey_log'
 describe JourneyLog do
 
   subject(:journey_log) { described_class.new(journey_class: journey_class) }
-  let(:journey_class) { double :journey, new: entry_station }
+  let(:journey_class) { double :journey_class, new: journey }
+  let(:journey) { double :journey }
   let(:entry_station) { double :station }
+  let(:exit_station) { double :station }
 
   describe '#start' do
     it 'should return a journey with an entry station' do
-      expect(journey_log.start(entry_station)).to eq entry_station
+      expect(journey_log.start(entry_station)).to eq journey
     end
   end
+
+  describe '#finish' do
+    it 'records total journey' do
+      journey_log.start(entry_station)
+      expect(journey_log.finish(exit_station)).to eq({ :entry_station => entry_station , :exit_station => exit_station })
+    end
+  end
+
+
 
 end
